b8924f18adb8722580c8a4bd5f1c856453cc272e
Anatoly Duzenko <a.duzenko@wsmintl.com>
2024-11-13 12:56:15 -0500

    Booking views

--- LittleLemon/Restaurant/serializers.py
+++ LittleLemon/Restaurant/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Menu
+from .models import Menu, Booking
 
 class MenuItemSerializer(ModelSerializer):
     class Meta:
         model = Menu
         fields = ['Title', 'Price', 'Inventory']
+
+class BookingSerializer(ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = ['Name', 'No_of_guests', 'BookingDate']
