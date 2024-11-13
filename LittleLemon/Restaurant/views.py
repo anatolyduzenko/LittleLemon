1606aa00f6b12d63b8397efdbc3088cae8858100
Anatoly Duzenko <a.duzenko@wsmintl.com>
2024-11-13 12:10:24 -0500

    Added serializers and API views for Menu Items

--- LittleLemon/Restaurant/views.py
+++ LittleLemon/Restaurant/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
+from .serializers import MenuItemSerializer
+from .models import Menu
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemsView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+
+class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
